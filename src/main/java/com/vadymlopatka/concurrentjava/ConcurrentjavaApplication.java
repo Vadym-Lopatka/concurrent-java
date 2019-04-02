40c1d02b18723f0e4bdaa574c6229f579dc9274f
Vadym-Lopatka <mirve09021990lik>
2019-04-02 20:11:30 +0300

    init

---
+++ src/main/java/com/vadymlopatka/concurrentjava/ConcurrentjavaApplication.java
@@ -0,0 +1,13 @@
+package com.vadymlopatka.concurrentjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrentjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vadymlopatka/concurrentjava/ConcurrentjavaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vadymlopatka.concurrentjava;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConcurrentjavaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
